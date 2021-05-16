6738047d5bc776bbd72d03194140b6ddcc789610
Eun Jin <ouhouh2222@gmail.com>
2021-05-17 00:39:52 +0900

    a

--- src/controller/globalController.js
+++ src/controller/globalController.js
@@ -7,4 +7,41 @@ export const homeController = async (req, res) => {
   } = req;
 
   console.log(seq);
+  console.log(searchValue);
+
+  try {
+    if (!seq && searchValue) {
+      const books = await Books.find().populate({
+        path: `author`,
+        model: Author,
+      });
+      res.render("screens/home", { books: books });
+    } else {
+      if (seq === "title") {
+        const books = await Books.find({
+          title: { $regex: `.*${searchValue}.*` },
+        }).populate({
+          path: `author`,
+          model: Author,
+        });
+        res.render("screens/home", { books: books });
+      } else {
+        if (seq === "author") {
+          const books = await Books.find().populate({
+            path: `author`,
+            model: Author,
+            match: {
+              name: { regex: `.*${searchValue}.*` },
+            },
+          });
+          const nextBook = books.filter((data) => data.author !== aull);
+
+          res.render("screens/home", { books: nextBook });
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    res.render("screens/home", { books: [] });
+  }
 };
